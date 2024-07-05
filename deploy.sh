4b338111015757225c6180d1dd1d0b19bfaceedf
Lluís Bosch <holalluis@gmail.com>
2024-07-05 17:24:33 +0200

    update

--- deploy.sh
+++ deploy.sh
@@ -10,4 +10,7 @@ git push
 # -P  mostra progrés
 # -vv incrementa verbositat
 # -r  actua recursivament
-rsync --rsync-path="sudo rsync" -hPvvr . debian@51.91.250.220:/var/www/html/banderes
+rsync --rsync-path="sudo rsync" -hPvvr \
+  --exclude ".git" \
+  --exclude "img/" \
+  . debian@51.91.250.220:/var/www/html/banderes
